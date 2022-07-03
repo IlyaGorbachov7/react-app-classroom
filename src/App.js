b447772736d0c402b5f438a28fe0a31a05afc553
Ilyz <gorbacevaili891@gmail.com>
2022-07-03 22:13:22 +0300

    General commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const state = useState(0)
+    const [stat, setStat] = useState(0);
+    console.log(setStat)
+    function incrFun() {
+        count++;
+        console.log(count)
+        setStat(stat + 1)
+    }
+
+    return (
+        <div className="App">
+            <Login/>
+        </div>
+    );
+
 }
+
+let count = 5;
 
 export default App;
