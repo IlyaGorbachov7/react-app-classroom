566f64365f7e9b4bb88a140524d01961853e82cd
Ilyz <gorbacevaili891@gmail.com>
2022-07-04 18:20:00 +0300

    fixed problem view person

--- src/components/MainWindow.jsx
+++ src/components/MainWindow.jsx
@@ -6,31 +6,50 @@ import RowTable from "./RowTable";
 
 const MainWindow = () => {
     const [items, setItemsTable] = useState([
-        {id: 5, userName: "Ilya Gorbachev", isHand: "visible"},
-        {id: 4, userName: "Tomas Andersan", isHand: "visible"},
-        {id: 3, userName: "Romigo Panamera", isHand: "visible"}
+        {id: 1, userName: "Ilya Gorbachev", isHand: true, status: "student"},
+        {id: 2, userName: "Tomas Andersan", isHand: true, status: "header"},
+        {id: 3, userName: "Romigo Panamera", isHand: true, status: "student"}
     ]);
 
+    // Current logging consumer
+    const [curUser, setCurUser] = useState(items[1])
 
-    function btnViewAction(e) {
+    // Status current logging consumer
+    const [curStatus, setCurStatus] = useState(function () {
+        if (curUser.status === "student") {
+            return "hidden"
+        } else {
+            return "visible"
+        }
+    })
+
+    /**
+     * Main handler event cliked on the button "Rise Hand up"
+     */
+    function handlerRiseHand(e) {
         e.preventDefault()
-        document.getElementById("myDropdownAction").classList.toggle("show");
+        if (curUser.isHand === true) {
+            setCurUser({...curUser, isHand: false})
+            document.getElementById("idActionHand").innerHTML = "Dawn hand"
+        } else {
+            setCurUser({...curUser, isHand: true})
+            document.getElementById("idActionHand").innerHTML = "Rise hand up"
+        }
+
+        console.log(curUser)
+        //... Code by sending request on the server
     }
 
-    function btnViewUser(e) {
-        e.preventDefault()
-        document.getElementById("myDropdownUser").classList.toggle("show");
-    }
-
-    function handlerRiseHand(e){
-        e.preventDefault()
-        console.log("handRise")
-    }
-
-    function handlerLogout(e){
+    /**
+     * Main  handler event clicked on the button "Logout"
+     */
+    function handlerLogout(e) {
         e.preventDefault()
         console.log("logout")
+
+        //... Code by sending request on the server
     }
+
     // Close the dropdown if the user clicks outside of it
     window.onclick = function (event) {
         if (!event.target.matches('.dropbtn')) {
@@ -45,28 +64,39 @@ const MainWindow = () => {
             }
         }
     }
+
+    function btnViewAction(e) {
+        e.preventDefault()
+        document.getElementById("myDropdownAction").classList.toggle("show");
+    }
+
+    function btnViewUser(e) {
+        e.preventDefault()
+        document.getElementById("myDropdownUser").classList.toggle("show");
+    }
+
     return (
         <div>
             <div className="container">
                 <div className="dropdown-action">
                     <button onClick={btnViewAction} className="dropbtn">Actions</button>
                     <div id="myDropdownAction" className="dropdown-content"
-                        onClick={handlerRiseHand}>
-                        <div>Raise hand up</div>
+                         onClick={handlerRiseHand}>
+                        <div id="idActionHand">Raise hand up</div>
                     </div>
                 </div>
 
                 <div className="dropdown-user">
-                    <button onClick={btnViewUser} className="dropbtn">{items[0].userName}</button>
+                    <button onClick={btnViewUser} className="dropbtn">{curUser.userName}</button>
                     <div id="myDropdownUser" className="dropdown-content"
-                    onClick={handlerLogout}>
-                        <div>Logout</div>
+                         onClick={handlerLogout}>
+                        <div id="idLogout">Logout</div>
                     </div>
                 </div>
             </div>
 
             <div className="table-style">
-                {items.map(value => <RowTable item={value} key={value.id}/>)}
+                {items.map(value => <RowTable item={value} key={value.id} statusCurItem={curStatus}/>)}
             </div>
         </div>
     );
