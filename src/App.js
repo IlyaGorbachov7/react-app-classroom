18de231e66d5ad44bddada807684d0226f208e91
Ilyz <gorbacevaili891@gmail.com>
2022-07-05 11:33:45 +0300

    made a function callback to Login.jsx

--- src/App.js
+++ src/App.js
@@ -4,66 +4,30 @@ import MainWindow from "./components/MainWindow";
 
 function App() {
     // Current logging consumer
-    const [items, setItems] = useState([
+    const stateItems = useState([
         {id: 1, userName: "Ilya Gorbachev", isHand: true, status: false},
         {id: 2, userName: "Tomas Andersan", isHand: true, status: true},
         {id: 3, userName: "Romigo Panamera", isHand: true, status: false},
     ]);
 
-    const [loginData, setLoginData] = useState({
-        name: "",
-        status: false
-    });
 
-    const funSubmit = function (event) {
-        event.preventDefault()
-        console.log(loginData)
+    // make State by default - is current logging user
+    const stateCurUser = useState({id: Date.now(), userName: "", isHand: false, status: false})
 
-        const newItem = {
-            id: Date.now(),
-            userName: loginData.name,
-            status: loginData.status,
-            isHand: false
-        }
-        setItems([...items, newItem])
-        // Code send request on the server
+    const createUser = (newUser) => {
+        console.log(newUser)
+        stateCurUser[1](newUser)
+        stateItems[1]([...stateItems[0], newUser])
     }
-
     return (
         <div className="App">
-            <div className="form-popup" id="myForm">
-                <form className="form-container">
-                    <h1>Join class</h1>
-
-                    <label htmlFor="email"><b>Email</b></label>
-                    <input type="text" placeholder="Name Lastname"
-                           value={loginData.name}
-                           onChange={event => {
-                               setLoginData({...loginData, name: event.target.value})
-                           }
-                           }
-                           required/>
-
-                    <label>
-                        <input type="checkbox"
-                               checked={loginData.status}
-                               onChange={event => setLoginData({...loginData, status: event.target.checked})}/> You are
-                        the
-                        creator ?
-                    </label>
-
-                    <button type="button" className="btn" onClick={funSubmit}>Login</button>
-                </form>
-            </div>
-
-
-            <MainWindow items={items}/>
+            <Login create={createUser}/>
+            <MainWindow stateCurUser={stateCurUser} items={stateItems[0]}/>
 
         </div>
     );
 
 }
 
-let count = 5;
 
 export default App;
